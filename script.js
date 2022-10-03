8e82751f813feead88fa96326a770cc2a48660fc
Samuel A <al2193001733@azc.uam.mx>
2022-10-03 17:35:59 -0500

    add basic game logic

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const GameboardController = () => {
     updateBoard = function (position) {
         if (board[position] === " ") {
             board[position] = getCurrentPlayerSymbol()
+            if (checkGameOver()) {
+                return true;
+            }
             nextPlayerTurn()
             return true
         }
@@ -18,6 +21,33 @@ const GameboardController = () => {
         else {
             setCurrentPlayerSymbol("X");
         }
+    }
+
+    checkGameOver = function () {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        for (let i = 0; i < winningCombinations.length; i++) {
+            combo = winningCombinations[i];
+            if (board[combo[0]] !== " ") {
+                if (board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+                    console.log(board[combo[0]], "is the winner!")
+                    return true
+                }
+            }
+        }
+        if (board.includes(" ")) {
+            return false
+        }
+        console.log("DRAW")
+        return true
     }
 
     getCurrentPlayerSymbol = () => {
@@ -50,7 +80,6 @@ const Gameboard = () => {
 
     return null
 }
+
 gameboardController = GameboardController()
-
-
 gameboard = Gameboard()
