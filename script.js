69cbcf7ea678ac82db3355bb859b715492c6c07e
Samuel A <al2193001733@azc.uam.mx>
2022-10-04 19:41:40 -0500

    add player names and avatars

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const GameboardController = () => {
+const GameboardController = (() => {
 
     let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     let currentPlayerSymbol = "X"
 
-    updateBoard = (position) => {
+    const updateBoard = (position) => {
         if (board[position] === " ") {
             board[position] = getCurrentPlayerSymbol()
             nextPlayerTurn()
@@ -12,7 +12,7 @@ const GameboardController = () => {
         return false
     }
 
-    nextPlayerTurn = () => {
+    const nextPlayerTurn = () => {
         if (getCurrentPlayerSymbol() === "X") {
             setCurrentPlayerSymbol("O");
         }
@@ -21,7 +21,7 @@ const GameboardController = () => {
         }
     }
 
-    checkGameOver = () => {
+    const checkGameOver = () => {
         const winningCombinations = [
             [0, 1, 2],
             [3, 4, 5],
@@ -37,7 +37,8 @@ const GameboardController = () => {
             combo = winningCombinations[i];
             if (board[combo[0]] !== " ") {
                 if (board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
-                    message.innerHTML = ("<p>" + board[combo[0]] + " IS THE WINNER!</p>")
+                    let playerWinner = board[combo[0]] === "X" ? player1.getName() : player2.getName()
+                    message.innerHTML = ("<p>" + playerWinner + " IS THE WINNER!</p>")
                     message.classList.add("visible")
                     return true
                 }
@@ -51,36 +52,36 @@ const GameboardController = () => {
         return true
     }
 
-    reset = () => {
+    const reset = () => {
         board.fill(" ")
         setCurrentPlayerSymbol("X")
     }
 
-    getCurrentPlayerSymbol = () => {
+    const getCurrentPlayerSymbol = () => {
         return currentPlayerSymbol
     }
 
-    setCurrentPlayerSymbol = (symbol) => {
+    const setCurrentPlayerSymbol = (symbol) => {
         currentPlayerSymbol = symbol
     }
 
-    getSymbolInCell = (position) => {
+    const getSymbolInCell = (position) => {
         return board[position]
     }
 
     return { updateBoard, getSymbolInCell, checkGameOver, reset }
 
-}
+})();
 
-const Gameboard = () => {
+const Gameboard = (() => {
 
     let cells = document.querySelectorAll(".section")
 
     placeSymbol = (e) => {
         let cell = e.target
-        if (gameboardController.updateBoard(cell.id)) {
-            cell.innerHTML = "<p>" + gameboardController.getSymbolInCell(cell.id) + "</p>"
-            if (gameboardController.checkGameOver()) {
+        if (GameboardController.updateBoard(cell.id)) {
+            cell.innerHTML = "<p>" + GameboardController.getSymbolInCell(cell.id) + "</p>"
+            if (GameboardController.checkGameOver()) {
                 lockBoard()
             }
         }
@@ -94,13 +95,41 @@ const Gameboard = () => {
     document.querySelector("button").addEventListener('click', () => {
         let message = document.querySelector(".win-message")
         message.classList.remove("visible")
-        gameboardController.reset()
+        GameboardController.reset()
         cells.forEach((el) => el.innerHTML = " ")
         cells.forEach((el) => el.addEventListener('click', placeSymbol));
     })
 
     cells.forEach((el) => el.addEventListener('click', placeSymbol));
+})();
+
+const Player = (playerNumber) => {
+    let name = document.querySelector("#player" + playerNumber).textContent;
+
+    const getName = () => {
+        return name;
+    }
+
+    const setName = (newName) => {
+        if (newName.length !== 0) {
+            name = newName;
+        }
+    }
+
+    return { getName, setName }
 }
 
-gameboardController = GameboardController()
-gameboard = Gameboard()
+let changeName = (e) => {
+    let newName = e.target.textContent
+    if (e.target.id === "player1-name") {
+        player1.setName(newName)
+    }
+    else {
+        player2.setName(newName)
+    }
+}
+
+document.querySelectorAll(".player").forEach(el => el.addEventListener("keyup", changeName))
+
+let player1 = Player(1)
+let player2 = Player(2)
